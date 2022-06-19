cb99087d107fcc41f40a54c4b4af0592bc042397
VictorLuna-ISC <75042129+VictorLuna-ISC@users.noreply.github.com>
2022-06-19 18:20:22 -0500

    Adicion de CRUD.
    Co-Authored-By: LuisBA31 <74994999+LuisBA31@users.noreply.github.com>
    Co-Authored-By: AdrianV14 <100966592+AdrianV14@users.noreply.github.com>
    Co-Authored-By: AndreaIRJ <102549067+AndreaIRJ@users.noreply.github.com>

--- src/app/menu-admin/menu-admin.component.ts
+++ src/app/menu-admin/menu-admin.component.ts
@@ -2,7 +2,21 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { DataBaseService } from 'src/app/services/database.service';
 import { Router } from '@angular/router';
+import { collection, getDocs, getFirestore } from 'firebase/firestore';
+import { initializeApp } from 'firebase/app';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyBzNSjDqDMolBbWBx58eFDI8UYtJLjYyaw",
+  authDomain: "requiemgeekshop.firebaseapp.com",
+  projectId: "requiemgeekshop",
+  storageBucket: "requiemgeekshop.appspot.com",
+  messagingSenderId: "246133176804",
+  appId: "1:246133176804:web:bc564b8c33f60fec372ff1"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 @Component({
   selector: 'app-menu-admin',
   templateUrl: './menu-admin.component.html',
@@ -10,7 +24,17 @@ import { Router } from '@angular/router';
 })
 export class MenuAdminComponent implements OnInit {
 
+  id:any = "";
+
   noticia:any={
+    Autor: '',
+    Titulo: '',
+    Categoria: '',
+    Descripcion: '',
+    RutaImagen: '',
+    Fecha: ''
+  }
+  updateNoticia:any={
     Autor: '',
     Titulo: '',
     Categoria: '',
@@ -31,21 +55,47 @@ export class MenuAdminComponent implements OnInit {
     this.noticia.RutaImagen = this.noticia.Titulo + ".jpg";
     try{
       this.database.crear('Noticias',this.noticia);
+      let currentUrl = this.router.url;
+      this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+      this.router.onSameUrlNavigation = 'reload';
+      this.router.navigate([currentUrl]);
     }catch(e){
-      
+      console.log("Error al dar de alta la noticia");
     }
   }
 
   async leer(){
-    const querySnapshot = await this.database.obtenerTodos('Noticias');
-    /*querySnapshot.forEach((doc) => {
-      this.noticias.push({
-        Autor: ${doc.get("Autor")},
-        Titulo: ${doc.get("Titulo")},
-        Categoria: ${doc.get("Categoria")},
-        Fecha: ${doc.get("Fecha")}
-      });
-    });*/
-  }
+    const querySnapshot = await getDocs(collection(db, "Noticias"));
 
+    for(var i = 0; i<querySnapshot.docs.length ;i++){
+      this.noticias.push({
+        Id: querySnapshot.docs[i].id,
+        Autor: querySnapshot.docs[i].get("Autor"),
+        Titulo: querySnapshot.docs[i].get("Titulo"),
+        Categoria: querySnapshot.docs[i].get("Categoria"),
+        Fecha: querySnapshot.docs[i].get("Fecha")
+        });
+    }
+  }
+  eliminar(IdRegistro:any){
+    this.database.eliminar("Noticias",IdRegistro);
+    let currentUrl = this.router.url;
+      this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+      this.router.onSameUrlNavigation = 'reload';
+      this.router.navigate([currentUrl]);
+  }
+  mostrarFormulario(IdRegistro:any){
+    var div:any;
+    div = document.getElementById("actualizarNoticia");
+    div.style.display="block";
+    this.id = IdRegistro;
+  }
+  actualizar(){
+    this.updateNoticia.RutaImagen = this.updateNoticia.Titulo + ".jpg";
+    this.database.actualizar("Noticias",this.updateNoticia,this.id);
+      let currentUrl = this.router.url;
+        this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+        this.router.onSameUrlNavigation = 'reload';
+        this.router.navigate([currentUrl]);
+  }
 }
