fdf3b90bc2f763dcd5514a21d4ab7f95fe2d58a5
LuisBA <luisbeltranarroyo@hotmail.com>
2022-06-19 02:49:56 -0500

    Funcionamiento de Registro y Login
    Login funcionando e implementación de registro
    
    Co-Authored-By: AdrianV14 <100966592+AdrianV14@users.noreply.github.com>
    Co-Authored-By: VictorLuna-ISC <75042129+VictorLuna-ISC@users.noreply.github.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,22 +14,25 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { PhoneLoginComponent } from './phone-login/phone-login.component';
+import { HomeComponent } from './home/home.component';
 
 firebase.initializeApp(environment.firebase)
+
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    PhoneLoginComponent
+    PhoneLoginComponent,
+    HomeComponent
   ],
   imports: [
-    AngularFireModule,
     AngularFireAuthModule,
     AngularFirestoreModule,
     AngularFireStorageModule,
     AngularFireDatabaseModule,
     BrowserModule,
     FormsModule,
+    AngularFireModule.initializeApp(environment.firebase),
     AppRoutingModule
   ],
   providers: [],
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { DataBaseService } from 'src/app/services/database.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,11 +10,76 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  usuario:any = {
+    email: '',
+    password: '',
+  }
+
+  usuarios:any = {
+    NombreUsuario: '',
+    password: '',
+    Privilegio: '0',
+    email: '',
+    CategoriaFav: ''
+  }
+
+  titulo:string ="Actualidad";
+
+  constructor(private authService: AuthService, private database: DataBaseService, private router: Router) { }
 
   ngOnInit(): void {
+    this.database.obtenerTodos("Usuarios").subscribe((usuariosRef) => {
+      console.log("usuariosRef: ", usuariosRef);
+      this.usuarios = usuariosRef.map(userRef => {
+        let usuario: any = userRef.payload.doc.data();
+        usuario['id'] = userRef.payload.doc.id;
+        return usuario;
+      });
+      console.log(this.usuarios);
+    })
   }
-  public phoneClick() {
-    window.location.href="../../src/app/login-phone/login-phone.component.html";
+
+  cambio(tit : string){
+    this.titulo=tit;
   }
+
+  registrarse() {
+    this.usuarios.CategoriaFav = this.titulo;
+    const { email, password } = this.usuarios;
+    this.authService.register(email, password).then(user => {
+      console.log("se registro: ", user);
+      let lista = [...this.usuarios];
+      let existe = lista.find(user => this.usuarios.email == email);
+
+      if (!existe) {
+        console.log("USUARIO NUEVO CREADO")
+        this.database.crear('Usuarios', this.usuarios);
+      };
+
+      this.router.navigate(['/']);
+    }).catch(err => {
+      console.log(err)
+    })
+  }
+
+  ingresar() {
+    const { email, password } = this.usuario;
+    this.authService.login(email, password).then(user => {
+      console.log("Bienvenido ", user);
+      console.log(user?.user?.email);
+      if(!user) {
+        alert("Datos incorrectos, si no tenes cuenta registrate!");
+        return;
+      };
+      this.router.navigate(['/panelDeControl'])
+    }).catch(err=>{
+      console.log(err)
+    })
+  }
+
+
+  logout() {
+    this.authService.logout();
+  }
+
 }
